2ac806c9d6fd1d157d957cd20a99b412666e5af9
juansesu3 <juanseu3@hotmail.com>
2023-11-02 10:09:51 +0100

    FEATURE(Fonts)Designer Lorena Prelaz give us the fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,7 @@
-import "@/styles/globals.css";
+import "../styles/fonts.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker
-          .register("/service-worker.js")
-          .then((registration) => {
-            console.log("Service Worker registrado con éxito:", registration);
-          })
-          .catch((error) => {
-            console.log("Error al registrar el Service Worker:", error);
-          });
-      });
-    }
-  }, []);
-
   return <Component {...pageProps} />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,40 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Cat Math</title>
-        <meta
-          name="description"
-          content="Learn more about Juan Sebastian Suarez Ramirez, a full-stack web developer specialized in AI, deep learning, and machine learning."
-        />
+        <meta name="description" content="learning Math with Cat." />
 
         <meta property="og:title" content="Cat math Inicio" />
       </Head>
-      <div className="bg-primary ">Home</div>
+      <div className="flex justify-center items-center  min-h-screen bg-primary relative">
+        <div className="flex flex-col items-center justify-center gap-2  ">
+          <span className="text-bold transform rotate-[-6]">
+            <p className="text-medium text-3xl">
+              CAT
+            </p>
+          </span>
+          <span className="w-[280px]">
+            <Image
+              className="w-full"
+              src={"/icon-892x920.png"}
+              width={200}
+              height={100}
+              alt="Icon"
+            />
+          </span>
+
+          <span className="text-bold transform rotate-[-6]">
+            <p className="text-medium text-3xl ">MATH</p>
+          </span>
+          <span className="flex items-center justify-center border rounded-full w-4 h-4 p-10 bg-[#fee741]">
+            <p className="text-medium text-3xl">X</p>
+          </span>
+        </div>
+      </div>
     </>
   );
 }
