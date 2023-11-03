9c6f062e9d4dfb2fa849a962684545c247330f89
juansesu3 <juanseu3@hotmail.com>
2023-11-03 17:00:30 +0100

    FEATURE(game logic)

--- components/Game.tsx
+++ components/Game.tsx
@@ -11,16 +11,27 @@ type Level = {
   questions: Question[];
 };
 
+// Función para generar un número aleatorio dentro de un rango
+const getRandomNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 const generateLevels = (maxTableNumber: number): Level[] => {
   const levels: Level[] = [];
   for (let i = 0; i <= maxTableNumber; i++) {
     const questions: Question[] = [];
     for (let j = 1; j <= 10; j++) {
       const result = i * j;
-      // Generar opciones aleatorias para la pregunta
-      const options = [result - 1, result, result + 1].sort(
-        () => Math.random() - 0.5
-      );
+      // Generar opciones aleatorias para la pregunta, asegurando que la respuesta correcta esté incluida
+      let options = [result];
+      while (options.length < 3) {
+        let option = getRandomNumber(result - 10, result + 10);
+        if (options.indexOf(option) === -1 && option !== result) {
+          options.push(option);
+        }
+      }
+      // Mezclar las opciones para que la posición de la respuesta correcta sea aleatoria
+      options = options.sort(() => Math.random() - 0.5);
       questions.push({ multiplier: j, result, options });
     }
     levels.push({ tableNumber: i, questions });
@@ -34,7 +45,12 @@ const Game = () => {
   const levels = generateLevels(12);
   const currentLevel = levels[currentLevelIndex];
   const currentQuestion = currentLevel.questions[currentQuestionIndex];
-
+  const buttonColors = [
+    "bg-[#f78f2e]",
+    "bg-[#f14c90]",
+    "bg-[#fde642]",
+    "bg-[#3ab14b]",
+  ].sort(() => Math.random() - 0.5);
   const handleAnswer = (answer: number) => {
     if (answer === currentQuestion.result) {
       // Si la respuesta es correcta, ir a la siguiente pregunta
@@ -49,18 +65,31 @@ const Game = () => {
       alert("¡Respuesta incorrecta! Inténtalo de nuevo.");
     }
   };
+  const getButtonClass = (index: number) => {
+    if (index === 2) {
+      // Si es el tercer botón, ocupa las dos columnas
+      return "col-span-2";
+    }
+    return "";
+  };
 
   return (
-    <div>
+    <div className="min-h-screen  flex flex-col gap-6 items-center justify-center font-myFont font-bold  text-secondary">
       {/* Mostrar la pregunta actual */}
-      <h2>
+      <h2 className="text-5xl  ">
         {currentLevel.tableNumber} x {currentQuestion.multiplier} = ?
       </h2>
 
       {/* Mostrar las opciones */}
-      <div>
+      <div className="grid grid-cols-2 gap-3  justify-items-center align-items-center">
         {currentQuestion.options.map((option, index) => (
-          <button key={index} onClick={() => handleAnswer(option)}>
+          <button
+            className={`${
+              buttonColors[index]
+            } w-16 h-16 rounded-full  ${getButtonClass(index)}`}
+            key={index}
+            onClick={() => handleAnswer(option)}
+          >
             {option}
           </button>
         ))}
--- pages/game.tsx
+++ pages/game.tsx
@@ -1,7 +1,16 @@
 import Game from "@/components/Game";
-
+import { useEffect, useState } from "react";
 
 const GamePage = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <>
       <Game />
