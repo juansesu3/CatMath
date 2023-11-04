a2392c37606a1158b1ea5ac4307132221d74c179
juansesu3 <juanseu3@hotmail.com>
2023-11-04 18:43:08 +0100

    FEATURE(login and logut)is ready now i'm going to implement the time limit to respond questions and initialice the feature ponints to unlock next levels

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -26,7 +26,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       {children}
       <button
         onClick={logout}
-        className="bg-red-500 w-[25%] mb-2 cursor-pointer hover:bg-gray-400 px-2 py-1 w-[10%] m-auto text-white font-myFont flex items-center justify-center rounded-md mt-6 shadow-md transition duration-300 ease-in-out"
+        className="bg-red-500  mb-2 cursor-pointer hover:bg-gray-400 px-4 py-1  m-auto text-white font-myFont flex items-center justify-center rounded-md mt-6 shadow-md transition duration-300 ease-in-out"
       >
         logout
       </button>
