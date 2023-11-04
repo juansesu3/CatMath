c269621c95c8586ffb7e5af3c41fc46f0c68b926
juansesu3 <juanseu3@hotmail.com>
2023-11-04 16:50:20 +0100

    FEATURE(Login users)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,37 @@
 import React from "react";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import LoginForm from "./LoginForm";
+import { useRouter } from "next/router";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const router = useRouter();
+  const { pathname } = router;
   const { data: session } = useSession();
+
+  const logout = async () => {
+    await router.push("/");
+    await signOut();
+  };
 
   if (!session) {
     return <LoginForm />;
   }
 
-  return <div>{children}</div>;
+  return (
+    <div>
+      {children}
+      <button
+        onClick={logout}
+        className="bg-red-500 cursor-pointer hover:bg-gray-400 px-2 py-1 w-[10%] m-auto text-white font-myFont flex items-center justify-center rounded-md mt-6 shadow-md transition duration-300 ease-in-out"
+      >
+        logout
+      </button>
+    </div>
+  );
 };
 
 export default Layout;
